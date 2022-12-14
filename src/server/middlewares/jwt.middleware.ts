ff71c8c41a740ba459f8b0da8c7c5ceb3a4ea5fb
Wayne Huang <wayne123tw@gmail.com>
2022-12-15 01:26:07 +0800

    [FEATURE] add api exlude path

--- src/server/middlewares/jwt.middleware.ts
+++ src/server/middlewares/jwt.middleware.ts
@@ -7,12 +7,12 @@ const jwtMiddleware: RequestHandler = async (req, res, next) => {
     algorithms: ['HS256'],
   }).unless({
     path: [
-      '/v1/auth/logout',
-      '/v1/auth/register',
-      '/v1/auth/general-login',
-      '/v1/auth/social-login',
-      '/v1/auth/refresh-token',
-      '/v1/auth/forgot-password',
+      '/api/v1/auth/logout',
+      '/api/v1/auth/register',
+      '/api/v1/auth/general-login',
+      '/api/v1/auth/social-login',
+      '/api/v1/auth/refresh-token',
+      '/api/v1/auth/forgot-password',
     ],
   })(req, res, next)
 }
