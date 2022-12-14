b61b222a8a5021451041cf4347c038dfab974aca
Wayne Huang <wayne123tw@gmail.com>
2022-12-15 01:16:38 +0800

    [BUGFIX] fix api

--- src/pages/api/v1/[[...slug]].ts
+++ src/pages/api/v1/[[...slug]].ts
@@ -5,7 +5,6 @@ import jwtMiddleware from '../../../server/middlewares/jwt.middleware'
 import router from '../../../server/router'
 
 const sessionMiddleware = createSessionMiddleware()
-
 const handler = nc().use(corsMiddleware).use(sessionMiddleware).use(jwtMiddleware).use('/api/v1', router)
 
 export const config = {
--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,7 +1,10 @@
-import { Router } from 'express'
+import nc from 'next-connect'
 import authRouter from './auth'
 
-const router = Router()
+const router = nc()
+router.get('/', (req, res) => {
+  return res.end('Current endpoint: /v1')
+})
 router.use('/auth', authRouter)
 
 export default router
